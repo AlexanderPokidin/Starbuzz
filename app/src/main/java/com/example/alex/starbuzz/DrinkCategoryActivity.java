30519a7561472c920d1ff2f893a73c25a78cbe38
AlexanderPokidin <a.pokidin@gmail.com>
2017-07-06 12:50:03 +0300

    Starbuzz is done

--- app/src/main/java/com/example/alex/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/alex/starbuzz/DrinkCategoryActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -13,5 +15,12 @@ public class DrinkCategoryActivity extends ListActivity {
         ListView listDrinks = getListView();
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView listView, View itemView, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }

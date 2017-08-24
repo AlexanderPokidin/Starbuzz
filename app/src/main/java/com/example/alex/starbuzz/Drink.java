d07503f443113cb019448462ca8ae5fdf1dd6ab8
AlexanderPokidin <a.pokidin@gmail.com>
2017-08-24 22:38:36 +0300

    Database is done

--- app/src/main/java/com/example/alex/starbuzz/Drink.java
+++ app/src/main/java/com/example/alex/starbuzz/Drink.java
@@ -1,40 +1,42 @@
-package com.example.alex.starbuzz;
+        //The database replaced the Drink class
 
-/**
- * Created by ALEX on 01.07.2017.
- */
-
-public class Drink {
-    private String name;
-    private String description;
-    private int imageResourceId;
-
-    public static final Drink[] drinks = {
-        new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
-        new Drink("Cappuccino", "Espresso, hot milk, and steamed milk foam", R.drawable.cappuccino),
-        new Drink("Filter", "Haghest quality beans roasted and brewed fresh", R.drawable.filter)
-    };
-
-    private Drink(String name, String description, int imageResourceId) {
-        this.name = name;
-        this.description = description;
-        this.imageResourceId = imageResourceId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getImageResourceId() {
-        return imageResourceId;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-}
+//package com.example.alex.starbuzz;
+//
+///**
+// * Created by ALEX on 01.07.2017.
+// */
+//
+//public class Drink {
+//    private String name;
+//    private String description;
+//    private int imageResourceId;
+//
+//    public static final Drink[] drinks = {
+//        new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
+//        new Drink("Cappuccino", "Espresso, hot milk, and steamed milk foam", R.drawable.cappuccino),
+//        new Drink("Filter", "Haghest quality beans roasted and brewed fresh", R.drawable.filter)
+//    };
+//
+//    private Drink(String name, String description, int imageResourceId) {
+//        this.name = name;
+//        this.description = description;
+//        this.imageResourceId = imageResourceId;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public int getImageResourceId() {
+//        return imageResourceId;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.name;
+//    }
+//}
